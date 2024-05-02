25fac42df3b0f84b1b1e2e4e0662408b40e67954
nihar-rs <niharranjan.2621@gmail.com>
2024-05-02 21:09:08 +0530

    first commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,30 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.g.background = "dark"
+vim.opt.guicursor = ""
+vim.opt.swapfile = false
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 10
+
+vim.opt.nu = true
+vim.opt.rnu = true
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
