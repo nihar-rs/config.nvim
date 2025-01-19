24f81c0dfc6ba6903a856956e11c0e8a9d32cf90
Nihar Satapathy <nihar@Nihars-MacBook-Pro.local>
2025-01-19 22:29:10 +0530

    Jan2025_mods

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,17 +1,3 @@
---[[
-return {
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme "catppuccin-mocha"
-    end
-  }
-}
-]]
-
 return {
 	{
 	"ellisonleao/gruvbox.nvim",
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -32,7 +32,6 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.keymap.set('n', '<leader>a', 'ggVG')
-
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
