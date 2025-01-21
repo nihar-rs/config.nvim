6033ed23bcb4af02db1fe60da0891afe4158875c
Nihar Satapathy <nihar@Nihars-MacBook-Pro.local>
2025-01-21 22:30:52 +0530

    java_addition

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,50 +1,97 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = false,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      local util = require("lspconfig.util")
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = false,
+		},
+	},
+	{
+		"mfussenegger/nvim-jdtls",
+		lazy = false,
+		ft = "java",
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			local servers = {
+				bashls = true,
+				gopls = {
+					settings = {
+						gopls = {
+							hints = {
+								assignVariableTypes = true,
+								compositeLiteralFields = true,
+								compositeLiteralTypes = true,
+								constantValues = true,
+								functionTypeParameters = true,
+								parameterNames = true,
+								rangeVariableTypes = true,
+							},
+						},
+					},
+				},
+				lua_ls = {
+					server_capabilities = {
+						semanticTokensProvider = vim.NIL,
+					},
+				},
+				rust_analyzer = true,
+				pyright = true,
+				ts_ls = {
+					root_dir = require("lspconfig").util.root_pattern("package.json"),
+					single_file = false,
+					server_capabilities = {
+						documentFormattingProvider = false,
+					},
+				},
+				clangd = {
+					-- cmd = { "clangd", unpack(require("custom.clangd").flags) },
+					-- TODO: Could include cmd
+					init_options = { clangdFileStatus = true },
+					filetypes = { "c" },
+				},
+			}
+			local util = require("lspconfig.util")
 
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.clangd.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
 
-      lspconfig.ruff.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.ruff.setup({
+				capabilities = capabilities,
+			})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+			lspconfig.jdtls.setup({
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,6 +6,25 @@ return {
       local config = require("nvim-treesitter.configs")
       config.setup({
         auto_install = false,
+        ensure_installed = {
+          "bash",
+          "c",
+          "cpp",
+          "css",
+          "go",
+          "html",
+          "java",
+          "javascript",
+          "json",
+          "lua",
+          "markdown",
+          "python",
+          "rust",
+          "scss",
+          "typescript",
+          "vim",
+          "vimdoc",
+        },
         highlight = { enable = true },
         indent = { enable = true },
       })
