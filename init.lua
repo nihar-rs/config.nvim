68d5f3a5e9671b00ace78bff9dd871eb29b9c017
nihar-rs <niharranjan.2621@gmail.com>
2025-04-06 01:20:44 +0530

    March2025

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 print("Hello from Nihar")
 
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,25 +1,18 @@
 return {
 	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("lualine").setup({
-				options = {
-					icons_enabled = true,
-					theme = "auto",
-					component_separators = { left = "", right = "" },
-					section_separators = { left = "", right = "" },
-				},
-				sections = {
-					lualine_a = { "mode" },
-					lualine_b = { "branch", "diff", "diagnostics" },
-					lualine_c = { "filename" },
-					lualine_x = { "encoding", { "fileformat", symbols = { unix = "" } }, { "filetype" } },
-					lualine_y = { "progress" },
-					lualine_z = { "location" },
-				},
-				extensions = {},
-			})
-		end,
+	  "nvim-lualine/lualine.nvim",
+	  dependencies = { "nvim-tree/nvim-web-devicons" },
+	  config = function()
+	    require("lualine").setup({
+	      options = {
+	        icons_enabled = true,
+	        theme = "auto",
+	      },
+	      sections = {
+	        lualine_x = { "encoding", { "fileformat", symbols = { unix = "" } }, { "filetype" } },
+	      },
+	      extensions = {},
+	    })
+	  end,
 	},
 }
