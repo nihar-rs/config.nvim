8863498e42c48d4dd491274236701260065c5da2
nihar-rs <niharranjan.2621@gmail.com>
2025-04-11 19:41:19 +0530

    Suggestion decoration

--- init.lua
+++ init.lua
@@ -1,16 +1,15 @@
 print("Hello from Nihar")
 
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -18,23 +17,27 @@ require("vim-options")
 require("lazy").setup("plugins")
 
 --Highlight when yanking text
--- NOTE: cmd `yap || yip`
+
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking text",
-  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank({
-      higroup = "Substitute",
-      timeout = 100,
-    })
-  end,
+	desc = "Highlight when yanking text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "Substitute",
+			timeout = 100,
+		})
+	end,
 })
 
 vim.diagnostic.config({
-  virtual_text = true,
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-  severity_sort = true,
+	virtual_text = {
+		spacing = 4,
+		prefix = "●",
+    -- prefix = "⚠️",
+	},
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
 })
 
