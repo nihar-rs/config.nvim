6915a88216b91e30e091c94cbd28c58f840e467a
nihar-rs <niharranjan.2621@gmail.com>
2025-08-08 09:55:46 +0530

    sign_column transparency

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -1,14 +1,18 @@
 return {
-  {
-    "tpope/vim-fugitive"
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
 
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-    end
-  }
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+			vim.opt.signcolumn = "auto"
+			vim.cmd([[
+        highlight SignColumn guibg=#282828
+      ]])
+		end,
+	},
 }
